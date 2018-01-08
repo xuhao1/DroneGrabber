427b812f310f6c093851c4001aabcc13e77f419e
xuhao1 <xuhao3e8@gmail.com>
2018-01-08 15:41:05 +0800

    Add serial port function

--- mavlink_proxy.js
+++ mavlink_proxy.js
@@ -3,26 +3,59 @@
 const dgram = require('dgram');
 const server = dgram.createSocket('udp4');
 var drone_rinfo = 0;
+const SerialPort = require('serialport');
+const port = new SerialPort('/dev/ttyACM0', {autoOpen: false});
 
-server.on('error', function (err) {
-    console.log("server error", err.stack);
-    server.close();
-});
+const using_serial = true;
 
-server.on('message', function (msg, rinfo) {
-    // console.log("server got: msg",msg);
-    drone_rinfo = rinfo;
-    send_msg_to_gcs(msg);
-});
+if (using_serial) {
+    port.open(function (err) {
+        if (err) {
+            return console.log('Error opening port: ', err.message);
+        }
 
-server.on('listening', function () {
-    const address = server.address();
-    console.log("server listening ", address.address, address.port);
-});
+    });
 
-function send_mavlink2drone(data) {
-    if (drone_rinfo != 0)
-        server.send(data,drone_rinfo.port, drone_rinfo.address);
+    port.on('open', function () {
+        console.log('open port success')
+    });
+
+    port.on('data', function (data) {
+        console.log('Data:', data);
+        send_msg_to_gcs(msg);
+    });
+
+}
+else {
+
+
+    server.on('error', function (err) {
+        console.log("server error", err.stack);
+        server.close();
+    });
+
+    server.on('message', function (msg, rinfo) {
+        // console.log("server got: msg",msg);
+        drone_rinfo = rinfo;
+        send_msg_to_gcs(msg);
+    });
+
+    server.on('listening', function () {
+        const address = server.address();
+        console.log("server listening ", address.address, address.port);
+    });
+    server.bind(41234);
 }
 
-server.bind(41234);
+function send_mavlink2drone(data) {
+    if (using_serial) {
+        port.write(data);
+    }
+    else if (drone_rinfo != 0)
+        server.send(data, drone_rinfo.port, drone_rinfo.address);
+}
+
+
+
+
+
