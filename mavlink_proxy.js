95b03b38ecc3230d60cb7ff935dc3d45958692fe
xuhao1 <xuhao3e8@gmail.com>
2018-01-17 13:42:41 +0800

    BaudRate set to 57600

--- mavlink_proxy.js
+++ mavlink_proxy.js
@@ -4,7 +4,7 @@ const dgram = require('dgram');
 const server = dgram.createSocket('udp4');
 var drone_rinfo = 0;
 const SerialPort = require('serialport');
-const port = new SerialPort('/dev/ttyS0', {autoOpen: false});
+const port = new SerialPort('/dev/ttyS0', {autoOpen: false, baudRate: 57600});
 
 const using_serial = true;
 
